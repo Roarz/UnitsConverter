a3b664d38916d91acd99695b867f7cfb3b39fd18
Roarz <rorie.nmit@gmail.com>
2015-05-28 22:09:34 +1200

    Visual Basic Units Conversion Application
    Simple application written in VB.NET allowing 8 different conversion
    options between common units. This includes ounces to grams, litres to
    gallons, kilograms to stones, celsius to fahrenheit and vice versa.

---
+++ UnitsConverter/UnitsConverter/frmTableOutput.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTableOutput
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvwTableOutput = New System.Windows.Forms.ListView()
+        Me.btnClearResults = New System.Windows.Forms.Button()
+        Me.lblConversionChoice = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lvwTableOutput
+        '
+        Me.lvwTableOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvwTableOutput.GridLines = True
+        Me.lvwTableOutput.HideSelection = False
+        Me.lvwTableOutput.Location = New System.Drawing.Point(12, 28)
+        Me.lvwTableOutput.MultiSelect = False
+        Me.lvwTableOutput.Name = "lvwTableOutput"
+        Me.lvwTableOutput.Size = New System.Drawing.Size(256, 284)
+        Me.lvwTableOutput.TabIndex = 1
+        Me.lvwTableOutput.UseCompatibleStateImageBehavior = False
+        Me.lvwTableOutput.View = System.Windows.Forms.View.Details
+        '
+        'btnClearResults
+        '
+        Me.btnClearResults.Location = New System.Drawing.Point(193, 318)
+        Me.btnClearResults.Name = "btnClearResults"
+        Me.btnClearResults.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearResults.TabIndex = 2
+        Me.btnClearResults.Text = "Clear"
+        Me.btnClearResults.UseVisualStyleBackColor = True
+        '
+        'lblConversionChoice
+        '
+        Me.lblConversionChoice.AutoSize = True
+        Me.lblConversionChoice.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConversionChoice.Location = New System.Drawing.Point(13, 9)
+        Me.lblConversionChoice.Name = "lblConversionChoice"
+        Me.lblConversionChoice.Size = New System.Drawing.Size(109, 16)
+        Me.lblConversionChoice.TabIndex = 3
+        Me.lblConversionChoice.Text = "Selection Choice"
+        '
+        'frmTableOutput
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(286, 353)
+        Me.Controls.Add(Me.lblConversionChoice)
+        Me.Controls.Add(Me.btnClearResults)
+        Me.Controls.Add(Me.lvwTableOutput)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "frmTableOutput"
+        Me.Text = "Table Output"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lvwTableOutput As System.Windows.Forms.ListView
+    Friend WithEvents btnClearResults As System.Windows.Forms.Button
+    Friend WithEvents lblConversionChoice As System.Windows.Forms.Label
+End Class
---
+++ UnitsConverter/UnitsConverter/frmTableOutput.vb
@@ -0,0 +1,15 @@
+﻿Public Class frmTableOutput
+
+    Private Sub frmTableOutput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lvwTableOutput.Columns.Add("Value", 100, HorizontalAlignment.Left) 'Creates column in listview control to show value when the form is loaded
+        lvwTableOutput.Columns.Add("Result", 100, HorizontalAlignment.Left) 'Creates column in listview control to show result when the form is loaded
+    End Sub
+
+    Private Sub btnClearResults_Click(sender As Object, e As EventArgs) Handles btnClearResults.Click
+        lvwTableOutput.Items.Clear() 'When user clicks the clear button, the value and results data will be erased.
+    End Sub
+
+    Public Sub CalculationResult(ByRef prCounter As Decimal, ByRef prValue As Decimal) 'Receives counter and value parameter from 
+        lvwTableOutput.Items.Add(New ListViewItem({prCounter, prValue})) 'We will add the calculation to the listview control. This includes the value that was converted and the result.
+    End Sub
+End Class
